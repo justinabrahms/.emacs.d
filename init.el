9051244287bca3044d9fc41d078946a49a100b1c
Justin Lilly <justin@justinlilly.com>
2011-01-18 11:11:47 -0500

    more alterations

--- init.el
+++ init.el
@@ -6,35 +6,35 @@
 (global-unset-key (kbd "C-x m")) ; I don't use mail
 (global-unset-key (kbd "C-z")) ; suspending frame is useless with emacsclient and/or tmux
 
+(defalias 'qrr 'query-regexp-replace)
+(fset 'yes-or-no-p 'y-or-n-p)  ;; only type `y` instead of `yes`
+(setq inhibit-splash-screen t) ;; no splash screen
 
 ;; general programming things
-(show-paren-mode 1)
-
+(show-paren-mode 1)  ;; highlight matching parenthasis
+(menu-bar-mode -1) ;; minimal chrome
+(if (functionp tool-bar-mode)
+    (tool-bar-mode -1))
 
 ;; javascript
-(setq
- js-indent-level 2)
+(setq js-indent-level 2)
 
 ;; Autocomplete
-(add-to-list 'load-path "~/.emacs2/vendor/autocomplete/")
+(add-to-list 'load-path "~/.emacs.d/vendor/autocomplete/")
 (require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories "~/.emacs2/vendor/autocomplete/ac-dict")
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/vendor/autocomplete/ac-dict")
 (ac-config-default)
 
-;; ido-mode, fuzzy matching on find-file, buffer switch
-(ido-mode t)
+(ido-mode t);; fuzzy matching on find-file, buffer switch
 
-;; magit
-(add-to-list 'load-path "~/.emacs2/submodules/magit/")
+;; magit, interface to git via emacs
+(add-to-list 'load-path "~/.emacs.d/submodules/magit/")
 (require 'magit)
 
 ;; textile mode
-(add-to-list 'load-path "~/.emacs2/vendor/")
+(add-to-list 'load-path "~/.emacs.d/vendor/")
 (require 'textile-mode)
 (add-to-list 'auto-mode-alist '("\\.textile" . textile-mode))
-
-(menu-bar-mode -1)
-(tool-bar-mode -1)
 
 (setq auto-mode-alist
       (append
@@ -43,3 +43,38 @@
        auto-mode-alist))
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
+;; paredit?
+;; tempfiles, stolen from github://defunkt/emacs
+(defvar user-temporary-file-directory
+  (concat temporary-file-directory user-login-name "/"))
+(make-directory user-temporary-file-directory t)
+(setq backup-by-copying t
+      backup-directory-alist `(("." . ,user-temporary-file-directory))
+      auto-save-list-file-prefix (concat user-temporary-file-directory ".auto-saves-")
+      auto-save-file-name-transforms `((".*" ,user-temporary-file-directory)))
+
+;;; epackage.el
+;; One big file to boot all installed packages
+;; Automatically generated. Do not edit.
+(add-to-list 'load-path "~/.emacs.d/vendor/epackage/")
+(load "~/.emacs.d/00conf/epackage-loader" 'noerr)
+
+;;  M-x epackage to start package manager
+(autoload 'epackage "epackage" "" t)
+
+(autoload 'epackage-loader-file-byte-compile    "epackage" "" t)
+(autoload 'epackage-loader-file-generate        "epackage" "" t)
+(autoload 'epackage-cmd-autoload-package        "epackage" "" t)
+(autoload 'epackage-cmd-enable-package          "epackage" "" t)
+(autoload 'epackage-cmd-disable-package         "epackage" "" t)
+(autoload 'epackage-cmd-activate-package        "epackage" "" t)
+(autoload 'epackage-cmd-deactivate-package      "epackage" "" t)
+(autoload 'epackage-cmd-clean-package           "epackage" "" t)
+(autoload 'epackage-cmd-remove-package          "epackage" "" t)
+(autoload 'epackage-cmd-upgrade-package         "epackage" "" t)
+(autoload 'epackage-cmd-upgrade-all-packages    "epackage" "" t)
+(autoload 'epackage-cmd-download-sources-list   "epackage" "" t)
+(autoload 'epackage-cmd-download-package        "epackage" "" t)
+(autoload 'epackage-initialize                  "epackage" "" t)
+(autoload 'epackage-version                     "epackage" "" t)
+(autoload 'epackage-documentation               "epackage" "" t)
